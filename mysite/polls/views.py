896d3760a6e339be76803e17dd6a639291e4e1da
jace <jacealan1@gmail.com>
2017-12-07 09:28:02 +0900

    https://docs.djangoproject.com/ko/2.0/intro/tutorial05/

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -29,7 +30,9 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 # def detail(request, question_id):
 #     # return HttpResponse("You're looking at question %s." % question_id)
@@ -45,6 +48,9 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # def results(request, question_id):
 #     # response = "You're looing at the result of question %s."
